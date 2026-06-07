2a05412e41fb13826a962d057247abf7d7da1594
Federico Yarza <federicoy@epidataconsulting.com>
2026-06-07 18:19:58 -0400

    Add sitemap integration and external links component: updated Astro configuration to include sitemap generation, added new ExternalLinks component for social profiles, and updated pages to utilize the new component for improved navigation and contact options.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fyarza.github.io",
   base: "porfolio-minimalista",
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("/ats"),
+    }),
+  ],
 });
